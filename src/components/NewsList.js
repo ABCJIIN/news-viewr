b42203aaf394d072cc77da5983ee05ed67ee593a
ABCJIIN <wldls0309@naver.com>
2022-05-04 18:57:00 +0900

    first commit

---
+++ src/components/NewsList.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import NewsItem from "./NewsItem";
+import axios from "axios";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+// const sampleArticle = {
+//   title: "제목",
+//   description: "내용",
+//   url: "https://www.google.com",
+//   urlToImage: "http://via.placeholder.com/160.png",
+// };
+
+function NewsList({ category }) {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const query = category === "all" ? "" : `&category=${category}`;
+        const response = await axios.get(
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a346d3fbd10a4d39a8943cd33569a8d0`
+        );
+        setArticles(response.data.articles);
+      } catch (e) {}
+      setLoading(false);
+    };
+    fetchData();
+  }, [category]);
+
+  //대기중일 때
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+
+  //articles의 값이 아직 설정이 안됐을 때
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+}
+
+export default NewsList;
